506b1198f15e38bba9e64661b82e89243140ef43
Cristopher Salazar <salazar6cristopher@gmail.com>
2022-01-27 22:04:29 +0000

    Deployign to tedusoft-live-web

---
+++ _next/static/chunks/pages/extensions/[path]-9c7775ec93b70401.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[967],{16566:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/extensions/[path]",function(){return u(50777)}])}},function(n){n.O(0,[774,662,789,80,334,856,511,650,66,818,501,777,888,179],(function(){return _=16566,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
