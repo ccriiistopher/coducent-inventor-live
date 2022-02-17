7245896138df841e0c4d95d30b87b8eebfc03278
Cristopher Salazar <salazar6cristopher@gmail.com>
2022-02-17 14:36:27 +0000

    Deployign to tedusoft-live-web

---
+++ _next/static/kcs5RHEhvahAK128QHiUi/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdocs","\u002Fextensions","\u002Fhelp","\u002F","\u002Fdocs\u002F[path]","\u002Fextensions\u002F[path]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
